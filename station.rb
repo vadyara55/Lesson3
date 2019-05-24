392aef717f4617f4d3168a2ceed2c4a7fd1e42d1
vadyara55 <46928549+vadyara55@users.noreply.github.com>
2019-05-24 22:46:36 +0600

    change

---
+++ station.rb
@@ -0,0 +1,21 @@
+class Station  
+
+attr_reader :name, :trains
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def get_train(train)
+    trains << train
+  end
+
+  def send_train(train)
+    trains.delete(train)
+  end
+
+  def trains_by_type(type)
+    trains.select { |train| train.type == type }
+  end
+end
